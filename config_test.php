85c9193483a70242711406bd6c232db475baa3cd
pavand239 <pavand239@gmail.com>
2020-11-30 12:48:35 +0300

    конфиг для тестов

---
+++ config_test.php
@@ -0,0 +1,60 @@
+<?php
+
+$config = [
+    'id' => 'rest-fee-test',
+    // basePath (базовый путь) приложения будет каталог `rest-fee`
+    'basePath' => __DIR__,
+    // это пространство имен где приложение будет искать все контроллеры
+    'controllerNamespace' => 'restFee\controllers',
+    'aliases' => [
+        '@restFee' => __DIR__,
+    ],
+    'defaultRoute' => 'fee/index',
+    'bootstrap' => ['log','config'],
+    'components' => [
+        'user' => [
+            'identityClass' => 'restFee\models\User',
+        ],
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'test',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
+        ],
+        'cache' => [
+            'class' => 'yii\caching\DbCache',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=restFee_test',
+            'username' => 'pavand239',
+            'password' => 'P@ssw0rd',
+            'charset' => 'utf8',
+        ],
+        'config' => [
+            'class' => 'restFee\components\Config',
+        ],
+    ],
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
